9a54de5f533555f9fd627c17fb71e5af6f301cb6
Jiin <neri.barnini@gmail.com>
2015-02-15 15:03:36 +0100

    version v0.0.2

--- README.md
+++ README.md
@@ -16,14 +16,39 @@ Include the readingtime.js script provided in bower_components and add `ngReadin
 Usage
 -------
 
+You can use as directive:
+
 ```html
 <p>
   Reading time: <span ng-reading-time text-to-read="text"></span>
 </p>
 ```
 
+or as service:
+```javascript
+.directive('myCustomDirective', ['readingTime', function(readingTime) {
+  return {
+    // ... skipped code
+    link: function() {
+      scope.result = readingTime.get(text, {
+        wordsPerMinute: 210,
+        format: 'text_only'
+      });
+    }
+  }
+}]);
+```
+
+This directive has also two customizable attributes: _wordsPerMinute_ and _format_.
+The first represents the words that the user can reads in a minute (default is 210) and the second is the output format.
+At the moment ng-readingtime supports only 2 formats:
+* text_only: string like "readable in 5 minutes and 20 seconds"
+* value_only: object with minutes and seconds
+
+
+
 Todo
 -------
 
-* Add attribute to define custom words per minute
-* Add different kind of output (textual, graphical)
+* Add internationalization to textual output
+* Add graphical output
--- readingtime.js
+++ readingtime.js
@@ -1,12 +1,57 @@
 'use strict';
 
 angular.module('ngReadingTime', [])
-  .directive('ngReadingTime', function() {
+  .service('readingTime', function() {
+    var formats = {
+      value_only: function(time) {
+        return time;
+      },
+
+      text_only: function(time) {
+        var result = 'readable in ';
+
+        if (!time.minutes && !time.seconds)
+          result += 'a moment';
+
+        if (time.minutes)
+          result += time.minutes + ' minutes ';
+
+        if (time.seconds)
+          result += time.seconds + ' seconds';
+
+        return result;
+      }
+    };
+
+    return {
+      get: function(text, custom) {
+        if (!text) text = '';
+
+        var words = text.trim().split(/\s+/g).length,
+            wps   = (custom.wordsPerMinute || 210) / 60,
+            rts   = words / wps;
+
+        var format = (custom.format || 'text_only');
+
+        var m = Math.floor(rts / 60),
+            s = Math.round(rts - m * 60);
+
+        var out = formats[format].call(this, {
+          minutes: m,
+          seconds: s
+        });
+
+        return out;
+      }
+    }
+  })
+  .directive('ngReadingTime', ['readingTime', function(readingTime) {
     return {
       restrict: 'A',
       scope: {
         textToRead: '=',
-        wordsPerMinute: '@?'
+        wordsPerMinute: '@?',
+        format: '@?'
       },
       replace: true,
       template: '<span>{{txt}}</span>',
@@ -15,40 +60,12 @@ angular.module('ngReadingTime', [])
         scope.$watch(function() {
           return scope.textToRead;
         }, function (text) {
-
-          if (!text) text = '';
-
-          var words = text.trim().split(/\s+/g).length,
-              wps   = (scope.wordsPerMinute || 210) / 60,
-              rts   = words / wps;
-
-          var formats = {
-            text_only: function(time) {
-              var result = 'readable in ';
-
-              if (!time.minutes && !time.seconds)
-                result += 'a moment';
-
-              if (time.minutes)
-                result += time.minutes + ' minutes ';
-
-              if (time.seconds)
-                result += time.seconds + ' seconds';
-
-              return result;
-            }
-          };
-
-          var m = Math.floor(rts / 60),
-              s = Math.round(rts - m * 60);
-
-          var out = formats.text_only.call(this, {
-            minutes: m,
-            seconds: s
+          scope.txt = readingTime.get(text, {
+            wordsPerMinute: scope.wordsPerMinute,
+            format: scope.format
           });
-
-          scope.txt = out;
         });
+
       }
     }
-  });
+  }]);
